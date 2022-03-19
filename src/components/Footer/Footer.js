5532d660c5c9bd5e48b02d32e344a95d1da57caf
TempUsername2436 <christopherpokemon30@gmail.com>
2022-03-19 10:02:45 -0700

    Updated the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,6 @@
 function Footer() {
   return (
     <footer>
-      <h5>Two possibilities exist: either we are alone in the universe or we are not.<br />Both are equally terrifying.</h5>
     </footer>
   );
 }
